f979a786b8af5ee5a82078d5c4e99445661b8e6f
lorendiloreto <lorendiloreto@live.com>
2021-10-08 14:52:58 -0400

    Created React Boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
-    const name = props.name;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-        <button type="button" class="btn btn-primary">
-          This is a bootstrap button
-        </button>
-      </>
-    );
+  const name = props.name;
+  const [clicks, setClicks] = useState(0); /* State Hook used for keeping track of the component's "clicks" */
+
+  function sendRequest() { /* Simple Javascript function inside of component used for sending post request */
+    setClicks(clicks + 1) /* Used to set the value of the state "clicks" */
+    fetch( '/exampleRequest', { method:'post', 'no-cors':true })
+      .then( response => {
+        console.log("Request returned")
+      })
+  }
+  /* Returns HTML code using variables from this component */
+  return (
+    <>
+      <h1>
+        Hello {name}
+      </h1>
+      <h2>
+        Clicks: {clicks}
+      </h2>
+      <button type="button" class="btn btn-primary" onClick={sendRequest}>
+        This is a bootstrap button
+      </button>
+    </>
+  );
 }
 
 export default App;
