467fe0e81e40277fb392fe53a7dd8e63160c6b1c
msacristanbenjet <msacristanbenjet@agmednet.com>
2021-10-09 18:45:49 -0400

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ function App(props) {
     const name = props.name;
     const firstName = props.firstName;
     const lastName = props.LastName;
-    const mayor = props.mayor;
+    const major = props.major;
     const gpa = props.gpa;
     const image = props.image;
     const aboutMe = props.image;
     const resume = props.resume;
+    const year = props.year;
 
     const [clicks, setClicks] = useState(0); /* State Hook used for keeping track of the component's "clicks" */
     const [login, setLogin] = useState("LOGIN"); 
@@ -28,15 +29,19 @@ function App(props) {
 
     function FullName(props) {
         return <h5 className="card-title">{props.firstName} {props.lastName}</h5>;
-        return <td>{props.lastName}</td>;
+
     }
 
-    function Mayor(props) {
-        return <h6 className="card-subtitle mb-2 text-muted">{props.mayor}</h6>;
+    function Major(props) {
+        return <h6 className="card-subtitle mb-2 text-muted">{props.major}</h6>;
     }
 
     function GPA(props) {
-        return <h6 className="card-subtitle mb-2 text-muted right">   GPA:{props.gpa}</h6>
+        return <h6 className="d-inline-block card-subtitle mb-2 text-muted">   GPA:{props.gpa}</h6>
+    }
+
+    function Year(props) {
+        return <h6 className="d-inline-block card-subtitle mb-2 text-muted">Class of {props.year}</h6>
     }
 
     function Image(props) {
@@ -81,10 +86,12 @@ if(login=="LOGIN"){setLogin("SIGNUP")}else{setLogin("LOGIN")}
             <Image image="profile.jpg"/> 
             <div className="card-body">
                 <FullName firstName="Maria" lastName="Sacristan"/> 
-                <Mayor mayor="Computer Science"/>
-                 
+                <Major major="Computer Science"/>
+                <Year year="2022"/>
                 <AboutMe aboutMe="Some quick example text to build on the card title and make up the bulk of the card's content."/>
                 <GPA gpa="3.4"/>
+                
+                <br/>
                 <Resume resume="functionalsample.pdf"/>
             </div>
          </div>
